33a0b312e3caff055c12a7297a5a6b8164ae1a79
root <calebmarani@gmail.com>
2022-03-15 15:27:08 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,6 @@ int main(void)
 	 * The code utilizes printf command
 	 */
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
