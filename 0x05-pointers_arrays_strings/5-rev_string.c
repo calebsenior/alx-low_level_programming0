686039c37d550fc5965f5e8d5be6ba9e16a4ba61
root <calebmarani@gmail.com>
2022-03-21 17:31:04 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,16 +11,14 @@ int l, i, h;
 char t;
 
 for (l = 0; s[l] != '\0'; l++);
-
 i = 0;
-
 h = l / 2;
 
 while (h--)
 {
-        t = s[l - i - 1];
-        s[l - i - 1] = s[i];
-        s[i] = t;
-        i++;
+t = s[l - i - 1];
+s[l - i - 1] = s[i];
+s[i] = t;
+i++;
 }
 }
