f2bb74d6767afa839082020f0fca37628cb99b6e
root <calebmarani@gmail.com>
2022-03-25 00:27:19 -0700

    strcmp

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- *   * print_number - print numbers chars
- *     * @n: integer params
- *       * Return: 0
- *        **/
+ *  print_number - print numbers chars
+ *  @n: integer params
+ *  Return: 0
+ * 
+ */
 
 void print_number(int n)
 {
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * _strcmp - compare
- *   * @s1 : pointerto char params
- *    * @s2 : pointer to char params
- *     * Return: *dest
- *      */
+ *  _strcmp - compare
+ *  @s1 : pointerto char params
+ *  @s2 : pointer to char params
+ *  Return: *dest
+ */
 
 int _strcmp(char *s1, char *s2)
 {
